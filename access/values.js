e106b34c9ee3443eb050cef70577d9d34c48f0fc
xofo-rpi <83887473+xofo-rpi@users.noreply.github.com>
2022-11-25 21:46:43 +0100

    Update values.js

--- access/values.js
+++ access/values.js
@@ -3,15 +3,15 @@ var isRightsEnabled = false; // Права
 var isCovidCertificateEnabled = false; // COVID-сертификат
 
 // Общее
-var birthdate = "07.08.2002"; // Дата рождения
-var fullname = "Бacкaкoв Hикитa" // ФИО
-var name = "Микитa" // Имя
+var birthdate = "12.01.2003"; // Дата рождения
+var fullname = "Веспуччи Иван" // ФИО
+var name = "Иван" // Имя
 
 // Паспорт
-var passport_id = "1197643088" // Номер паспорта
+var passport_id = "1197643058" // Номер паспорта
 
 // КПП
-var kpp_id = "85493421" // ИНН
+var kpp_id = "85493771" // ИНН
 
 // Права
 var rights_valid_until = "20.08.2024" // Права "Дійсні до"
@@ -20,5 +20,5 @@ var rights_tsc = "ТСЦ 8631" // Права "Видав
 var rights_id = "KBE968639" // Права "Серія та номер""
 
 // COVID-сертификат
-var covid_valid_until = "01.01.2025" // Сертификат "Дійсний до"
+var covid_valid_until = "01.03.2025" // Сертификат "Дійсний до"
 var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
